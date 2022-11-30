437fa7a4c294e8076f2482cf6131b01538bace65
Boros Levente <88634980+FUKUNEPTUN@users.noreply.github.com>
2022-11-30 12:57:32 +0100

    xyí
    yxí

--- backend.js
+++ backend.js
@@ -14,18 +14,18 @@ app.listen(port, () => {
   console.log(`http://localhost:${port}/`)
 })
 
-app.get('/konyv', (req, res) => {
+app.get('/iro', (req, res) => {
 
     const connection = mysql.createConnection({
       host: 'localhost',
       user: 'root',
       password: '',
-      database: 'konyvtarbl'
+      database: 'library'
     })
     
     connection.connect()
     
-    connection.query('SELECT konyv.konyv_nev,iro.iro_nev,mufaj.mufaj_nev,konyv.konyv_oldalszam,konyv.konyv_kiadaseve,konyv.konyv_kep FROM `konyv` INNER JOIN iro on iro.iro_id=konyv.konyv_iro INNER JOIN mufaj ON mufaj.mufaj_id = konyv.konyv_mufaj ', (err, rows, fields) => {
+    connection.query('SELECT * FROM `iro_profil` ', (err, rows, fields) => {
       if (err) throw err
       res.send(rows)
     })
