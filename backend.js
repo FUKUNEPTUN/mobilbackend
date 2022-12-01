f81abb11ade8c6d98836402f39782ee887e5a3ae
Boros Levente <88634980+FUKUNEPTUN@users.noreply.github.com>
2022-12-01 11:45:10 +0100

    2022.12.01

--- backend.js
+++ backend.js
@@ -3,6 +3,18 @@ var cors = require('cors')
 const mysql = require('mysql')
 const app = express()
 const port = 3000
+var connection 
+function kapcsolat(){
+connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'library'
+})
+connection.connect()
+}
+
+
 app.use(express.static('kepek2'))
 app.use(express.json())
 app.use(cors())
@@ -15,21 +27,18 @@ app.listen(port, () => {
 })
 
 app.get('/iro', (req, res) => {
-
-    const connection = mysql.createConnection({
-      host: 'localhost',
-      user: 'root',
-      password: '',
-      database: 'library'
-    })
-    
-    connection.connect()
-    
+  kapcsolat()  
     connection.query('SELECT * FROM `iro_profil` ', (err, rows, fields) => {
       if (err) throw err
       res.send(rows)
     })
-    
     connection.end()
-    
+  })
+  app.get('/mufaj', (req, res) => {
+    kapcsolat()
+    connection.query('SELECT * FROM `mufaj` ', (err, rows, fields) => {
+      if (err) throw err
+      res.send(rows)
+    })
+    connection.end()
   })
