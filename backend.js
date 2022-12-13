7978eee0bd534bc867823e06e7b659ab6e316f4a
Boros Levente <88634980+FUKUNEPTUN@users.noreply.github.com>
2022-12-13 13:14:11 +0100

    iroprofil mukodik

--- backend.js
+++ backend.js
@@ -31,7 +31,7 @@ app.listen(port, () => {
 
 app.get('/iro', (req, res) => {
   kapcsolat()  
-    connection.query('SELECT * FROM `iro_profil` ', (err, rows, fields) => {
+    connection.query('SELECT * FROM iro_profil ORDER BY `iro_profil`.`iro_neve` ASC', (err, rows, fields) => {
       if (err) throw err
       res.send(rows)
     })
@@ -39,7 +39,7 @@ app.get('/iro', (req, res) => {
   })
   app.post('/iroprofil', (req, res) => {
 kapcsolat()
-    connection.query('SELECT iro_profil.iro_id,iro_profil.iro_kep,iro_profil.iro_leiras,iro_profil.iro_neve FROM iro_profil WHERE iro_profil.iro_neve LIKE "%'+req.body.bevitel1+'%"', function (err, rows, fields) {
+    connection.query('SELECT iro_profil.iro_id,iro_profil.iro_kep,iro_profil.iro_leiras,iro_profil.iro_neve FROM iro_profil WHERE iro_profil.iro_id = '+req.body.bevitel1, function (err, rows, fields) {
       if (err) 
         console.log( err)
       else{
@@ -53,7 +53,7 @@ kapcsolat()
   })
   app.post('/irokonyv', (req, res) => {
     kapcsolat()
-        connection.query('SELECT iro_profil.iro_id,iro_profil.iro_kep,iro_profil.iro_leiras,konyv_profil.kp_kep,iro_profil.iro_neve,konyv_profil.konyv_cime FROM iro_profil INNER JOIN konyv_profil ON konyv_profil.iro_id=iro_profil.iro_id WHERE iro_profil.iro_neve  LIKE "%'+req.body.bevitel1+'%"', function (err, rows, fields) {
+        connection.query('SELECT iro_profil.iro_id,iro_profil.iro_kep,iro_profil.iro_leiras,konyv_profil.kp_kep,iro_profil.iro_neve,konyv_profil.konyv_cime FROM iro_profil INNER JOIN konyv_profil ON konyv_profil.iro_id=iro_profil.iro_id WHERE iro_profil.iro_id = '+req.body.bevitel1, function (err, rows, fields) {
           if (err) 
             console.log( err)
           else{
