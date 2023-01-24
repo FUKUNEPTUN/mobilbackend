d0731b892d82e081ad26a9711e7b50527629bdc0
Boros Levente <88634980+FUKUNEPTUN@users.noreply.github.com>
2023-01-24 12:14:36 +0100

    Update backend.js

--- backend.js
+++ backend.js
@@ -124,7 +124,7 @@ app.get('/login', (req, res) => {
 })
 app.post('/tagprofil', (req, res) => {
   kapcsolat()
-  connection.query('SELECT * FROM tag_profil', function (err, rows, fields) {
+  connection.query('SELECT * FROM tag_profil WHERE tag_profil.tp_id = '+req.body.tagprofilid, function (err, rows, fields) {
     if (err)
       console.log(err)
     else {
@@ -140,7 +140,7 @@ app.post('/tagprofil', (req, res) => {
 
 app.post('/tagprofilkonyv', (req, res) => {
   kapcsolat()
-  connection.query('SELECT * FROM kolcsonzes INNER JOIN konyv_profil AS fika ON fika.kp_id = kolcsonzes.kp_id INNER JOIN tag_profil AS akif on kolcsonzes.tp_id = akif.tp_id  ', function (err, rows, fields) {
+  connection.query('SELECT * FROM kolcsonzes INNER JOIN konyv_profil AS fika ON fika.kp_id = kolcsonzes.kp_id INNER JOIN tag_profil AS akif on kolcsonzes.tp_id = akif.tp_id WHERE akif.tp_id ='+req.body.tagprofilid, function (err, rows, fields) {
     if (err)
       console.log(err)
     else {
@@ -155,7 +155,7 @@ app.post('/tagprofilkonyv', (req, res) => {
 })
 app.post('/foglalasupdate', (req, res) => {
   kapcsolat()
-  connection.query('UPDATE kolcsonzes SET k_lejar = "2023-01-28" WHERE kolcsonzes.k_id = 1 AND kolcsonzes.tp_id = 1', function (err, rows, fields) {
+  connection.query('UPDATE kolcsonzes SET k_lejar = "2023-01-28" WHERE kolcsonzes.k_id = '+req.body.kolcsid+' AND kolcsonzes.tp_id = '+req.body.tagprofilid, function (err, rows, fields) {
     if (err)
       res.send(result.affectedRows + " record(s) updated")
 
